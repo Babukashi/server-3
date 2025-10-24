bbc6e560f6bd1841bd730be730e3149e6c477a15
Babukashi <206505264+Babukashi@users.noreply.github.com>
2025-10-24 12:05:48 +0900

    henkou

--- server.js
+++ server.js
@@ -1,2 +1,13 @@
 import { Hono } from 'jsr:@hono/hono';
 import { serveStatic } from 'jsr:@hono/hono/deno';
+
+const app = new Hono();
+
+app.use('/*', serveStatic({ root: './public' }));
+
+Deno.serve(app.fetch);
+
+app.get('/api', (c) => {
+  console.log('aaaaaa');
+  return c.json({ message: 'Hello World!' });
+});
